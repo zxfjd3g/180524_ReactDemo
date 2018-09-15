8a108d39024c784a9da5fc9e248ec01b2f847cb0
zxfjd3g <258147149@qq.com>
2018-09-15 14:40:50 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,66 @@
 应用根组件
  */
 import React, {Component} from 'react'
-import {NavLink, Route, Switch, Redirect} from 'react-router-dom'
-
-import About from './views/About'
-import Home from './views/Home'
-import MyNavLink from './components/MyNavLink'
 
 
 export default class App extends Component {
 
+  state = {
+    count: 0
+  }
+
+  increment = () => {
+    const number = this.refs.number.value*1
+    const count = this.state.count + number
+    this.setState({
+      count
+    })
+  }
+
+  decrement = () => {
+    const number = this.refs.number.value*1
+    const count = this.state.count - number
+    this.setState({
+      count
+    })
+  }
+
+  incrementIfOdd = () => {
+    const number = this.refs.number.value*1
+
+    if(this.state.count%2===1) {
+      const count = this.state.count + number
+      this.setState({
+        count
+      })
+    }
+
+  }
+
+  incrementAsync = () => {
+    setTimeout(() => {
+      const number = this.refs.number.value*1
+      const count = this.state.count + number
+      this.setState({
+        count
+      })
+    }, 1000)
+  }
 
   render() {
+    const {count} = this.state
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <div className="page-header"><h2>React Router Demo</h2></div>
-          </div>
-        </div>
-
-
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/*导航路由链接*/}
-              <MyNavLink to="/about" className="list-group-item">About</MyNavLink>
-              <MyNavLink to="/home" className="list-group-item">Home</MyNavLink>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                {/*路由组件显示*/}
-                <Switch>
-                  <Route path='/about' component={About}/>
-                  <Route path='/home' component={Home}/>
-                  <Redirect to='/about'/>
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </div>
+        <p>click {count} times</p>
+        <select ref='number'>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select> &nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>increment if odd</button>&nbsp;
+        <button onClick={this.incrementAsync}>increment async</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {HashRouter, BrowserRouter} from 'react-router-dom'
 
 import App from './App'
 
 
-ReactDOM.render((
-  /*<BrowserRouter>
-    <App/>
-  </BrowserRouter>*/
-  <HashRouter>
-    <App/>
-  </HashRouter>
-), document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
